13a5bc5b5b4b6663a0a274f3444d44f5145f68b1
Jay Montal <Jaedwin.Montal@gmail.com>
2020-03-03 15:15:49 -0700

    Update sketch_mar03a.ino

--- Progress/sketch_mar03a/sketch_mar03a.ino
+++ Progress/sketch_mar03a/sketch_mar03a.ino
@@ -3,28 +3,44 @@
 #define STEPPER_PIN_2 10
 #define STEPPER_PIN_3 11
 #define STEPPER_PIN_4 12
+#define RESET_BUTTON 2
 int step_number = 0;
 int buttonState = 0;
 int count = 0;
-int rotation = 510;
+int count2 = 0;
+int rotation = 512;
 int testCount = rotation * (1/8);
 
 bool UP = true;
 bool DOWN = false;
 
 void setup() {
-pinMode(STEPPER_PIN_1, OUTPUT);
-pinMode(STEPPER_PIN_2, OUTPUT);
-pinMode(STEPPER_PIN_3, OUTPUT);
-pinMode(STEPPER_PIN_4, OUTPUT);
-
+  Serial.begin(9600);
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
+  pinMode(RESET_BUTTON, INPUT);
 }
 
 void loop() {
   
   while(count <= (rotation)){
-    move_up();
+    move_down();
     count++;
+  }
+
+  Serial.println(buttonState);
+  buttonState = digitalRead(RESET_BUTTON);  
+
+  if (buttonState == HIGH){
+    while(count2 <= count){
+      move_up();
+      count2++;
+    }
+    count = 0;
+    count2 = 0;
+  } else {
   }
 
 }
